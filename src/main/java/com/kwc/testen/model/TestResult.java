28588acc5698bf7fc0ffe55d8290e625b205ec08
Marius Kristensen <marius.kristensen@gmail.com>
2013-07-05 19:41:33 +0200

    Return mocked TestResult in JSON format

---
+++ src/main/java/com/kwc/testen/model/TestResult.java
@@ -0,0 +1,140 @@
+package com.kwc.testen.model;
+
+/**
+ * @author Marius Kristensen
+ */
+public class TestResult {
+    int id;
+    String brand;
+    String model;
+    String model_year;
+    String engine_cc;
+    int hp;
+    int nm;
+    String weight;
+    int passengers_num;
+    int speed;
+    String road_conditions;
+    String test_year;
+    String driver;
+
+    public TestResult(int id, String brand, String model, String model_year, String engine_cc, int hp, int nm, String weight, int passengers_num, int speed, String road_conditions, String test_year, String driver) {
+        this.id = id;
+        this.brand = brand;
+        this.model = model;
+        this.model_year = model_year;
+        this.engine_cc = engine_cc;
+        this.hp = hp;
+        this.nm = nm;
+        this.weight = weight;
+        this.passengers_num = passengers_num;
+        this.speed = speed;
+        this.road_conditions = road_conditions;
+        this.test_year = test_year;
+        this.driver = driver;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getModel_year() {
+        return model_year;
+    }
+
+    public void setModel_year(String model_year) {
+        this.model_year = model_year;
+    }
+
+    public String getEngine_cc() {
+        return engine_cc;
+    }
+
+    public void setEngine_cc(String engine_cc) {
+        this.engine_cc = engine_cc;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getNm() {
+        return nm;
+    }
+
+    public void setNm(int nm) {
+        this.nm = nm;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public int getPassengers_num() {
+        return passengers_num;
+    }
+
+    public void setPassengers_num(int passengers_num) {
+        this.passengers_num = passengers_num;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public String getRoad_conditions() {
+        return road_conditions;
+    }
+
+    public void setRoad_conditions(String road_conditions) {
+        this.road_conditions = road_conditions;
+    }
+
+    public String getTest_year() {
+        return test_year;
+    }
+
+    public void setTest_year(String test_year) {
+        this.test_year = test_year;
+    }
+
+    public String getDriver() {
+        return driver;
+    }
+
+    public void setDriver(String driver) {
+        this.driver = driver;
+    }
+}
--- src/main/java/com/kwc/testen/rest/TestRestService.java
+++ src/main/java/com/kwc/testen/rest/TestRestService.java
@@ -1,8 +1,11 @@
 package com.kwc.testen.rest;
 
+import com.kwc.testen.model.TestResult;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
 /**
@@ -11,11 +14,39 @@ import javax.ws.rs.core.Response;
 @Path("/")
 public class TestRestService {
 
+    public TestResult testResult;
+
+    @GET
+    @Path("/")
+    @Produces("application/json")
+    public TestResult printMessage() {
+        this.testResult = new TestResult(
+                1,
+                "VW",
+                "Golf",
+                "1999",
+                "2500",
+                250,
+                310,
+                "1400",
+                2,
+                175,
+                "Wet",
+                "2005",
+                "Reidar"
+        );
+        TestResult testResult = this.testResult;
+
+
+        return testResult;
+    }
+
     @GET
     @Path("/{param}")
-    public Response printMessage(@PathParam("param") String msg) {
-        String response = "Tada: " + msg;
-        return Response.status(Response.Status.OK).entity(response).build();
+    @Produces("application/json")
+    public TestResult getOne(@PathParam("param") String msg) {
+        // TODO get one
+        return null;
     }
 
 }
