81827333f9b26964b52a4059160c392724cb4f95
Marius Kristensen <marius.kristensen@gmail.com>
2013-08-08 22:17:16 +0200

    Added post method

--- src/main/java/com/kwc/testen/rest/TestRestService.java
+++ src/main/java/com/kwc/testen/rest/TestRestService.java
@@ -3,10 +3,13 @@ package com.kwc.testen.rest;
 import com.kwc.testen.db.TestResultRepository;
 import com.kwc.testen.model.TestResult;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -30,6 +33,16 @@ public class TestRestService {
     public TestResult getOneTestResult(@PathParam("param") int msg) throws SQLException {
         TestResultRepository repository = new TestResultRepository();
         return repository.getTestResult(msg);
+    }
+
+    @POST
+    @Path("/")
+    @Consumes("application/json")
+    public Response postTestResult(TestResult testResult) {
+
+        String result = "Test result added";
+        System.out.println("    ########    " + result);
+        return Response.status(201).entity(result).build();
     }
 
 }
